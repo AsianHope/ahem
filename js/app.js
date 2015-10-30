b750cac37d8d0b28023ecdefdc7b86f27d6f3e40
Solida Sang <ssang@asianhope.org>
2015-10-30 13:11:10 +0700

    add title

--- js/app.js
+++ js/app.js
@@ -8,30 +8,42 @@ var app =  angular.module('employeeList',['datatables','datatables.tabletools','
             $rootScope.showInclude = false;
             if($state.current.name === 'admin.staff') {
                 $rootScope.showInclude = true;
+                $rootScope.title = "View Employee";
             }
             else if($state.current.name === 'admin.profile') {
                 $rootScope.showInclude = true;
+                $rootScope.title = "Profile";
             }
             else if($state.current.name === 'admin.reports') {
                 $rootScope.showInclude = true;
+                $rootScope.title = "Expat Staff Report";
             }
             else if($state.current.name === 'admin.register') {
                 $rootScope.showInclude = true;
+                $rootScope.title = "Register";
             }
             else if($state.current.name === 'admin.MissingInfo') {
                 $rootScope.showInclude = true;
+                $rootScope.title = "Missing Information Report";
             }
             else if($state.current.name === 'admin.groups') {
                 $rootScope.showInclude = true;
+                $rootScope.title = "Groups";
             }
             else if($state.current.name === 'admin.inactiveStaffReport') {
                 $rootScope.showInclude = true;
+                $rootScope.title = "Inactive Staff Report";
             }
             else if($state.current.name === 'admin.documentReport') {
                 $rootScope.showInclude = true;
+                $rootScope.title = "Document Report";
             }
             else if($state.current.name === 'admin.employeeReport') {
                 $rootScope.showInclude = true;
+                $rootScope.title = "Employee Report";
+            }
+            else{
+              $rootScope.title = "Asian Hope Employee Management System";
             }
         });
 
